0973df74089b32433ad40e9b06e5e6d1054b865e
Abrard1 <abrar.dewan100@gmail.com>
2026-09-23 14:11:12 +0100

    Create casino.py
    Created the basic layout for the casino, a given balance at the start and other syntax to avoid the user from typing random numbers.

---
+++ casino.py
@@ -0,0 +1,86 @@
+import random 
+
+
+def main():
+    balance = 100
+
+    print("╔════════════════════════════╗")
+    print("║          🎲 DICE           ║")
+    print("╚════════════════════════════╝")
+
+    print(f"You have a balance of £{balance}")
+    
+    print()
+    
+    while True:
+        bet = float(input(f"How much £{balance} would you like to bet? "))
+        
+        if bet <= balance:
+            break
+    print()
+    
+    game = input("Which game would you like to play? \nDice \nCoin Flip \nSlots \nRock Paper Scissors \nExit? ")
+
+    if game == "Rock Paper Scissors":
+        rock_paper_scissors()
+    
+    
+def rock_paper_scissors():
+    
+
+    print()
+    print("Welcome to Rock Paper Scissors! The game is very simple.")
+    print("Rock beats Scissors, Scissors beats Paper and Paper beats Rock!")
+    
+    print("\n 1. Rock \n 2. Paper \n 3. Scissors")
+    
+    choice = int(input("\nEnter your choice please ")) 
+    
+    while choice > 3 or choice < 1:
+        choice = int(input("\nEnter a valid choice please"))
+    
+    if choice == 1:
+        choice_name = "Rock"
+    
+    elif choice == 2:
+        choice_name = "Paper"
+    
+    elif choice == 3:
+        choice_name = "Scissors"
+        
+    print()
+    
+    print(f"User has chosen {choice_name} awaiting dealers choice")
+    
+    print()
+    
+    for _ in range(3):
+        print("...")
+    
+    comp_choice = random.randint(1,3)
+    
+
+    if comp_choice == 1:
+        comp_choice_name = "Rock" 
+    
+    elif comp_choice == 2:
+        comp_choice_name = "Paper"
+        
+    else:
+        comp_choice_name = "Scissors"
+    
+    print()
+    
+    print(f"Dealer choice was, {comp_choice_name}")
+    print(f"{choice_name} vs {comp_choice_name}")
+    
+    if choice == comp_choice:
+        print("It's a draw money returned")
+    
+    if choice == 1 and comp_choice == 2:
+        print("Dealer has won!")
+    
+    
+
+
+main()
